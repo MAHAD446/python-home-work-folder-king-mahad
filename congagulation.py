e3d348579bb6126dfa506df17b50c9deb38c1429
mahad446 <mahadmubashir43@gmail.com>
2025-01-11 01:44:02 -0800

    CANGARAGULATION MESAGE HOME WORK.PY

---
+++ congagulation.py
@@ -0,0 +1,4 @@
+print("raj said:congrualations to mahad on getting a gold medal in state games ")
+print("mahad said: thanks")
+print("raj said:CONGURALATIONS to mahad on getting a gold medal in state games ")
+print("mahad said: thanks")
