a61fd7f6cd4b975ef805affd88a61a46663002ec
mahad446 <mahadmubashir43@gmail.com>
2025-01-11 02:10:48 -0800

    summer time.py

---
+++ summer times.py
@@ -0,0 +1,9 @@
+# get user input for the temperature
+temperature = float(input("enter the current temprature in degrees celsious"))
+
+#conditional statements to suggest clothing
+if temperature >25:
+    print("it's warm enough to wear light clothes.")
+else:
+    print("it's too cold, better stick to warm clothes.")
+    
